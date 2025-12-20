fc1a8fabc3c6e5fb46d45bf0f52744291976473f
Alexey Builov <builovalmaty@gmail.com>
2025-12-20 22:24:31 +0500

    step 1 ya ebal rot hexlet

--- task_manager/manage.py
+++ task_manager/manage.py
@@ -4,8 +4,8 @@ import os
 import sys
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent
-sys.path.append(str(BASE_DIR))
+BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.insert(0, str(BASE_DIR))
 
 def main():
     """Run administrative tasks."""
