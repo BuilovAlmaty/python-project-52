c686451ff94896bbdabc4b8dbb7a8c66a2c79a0d
Alexey Builov <builovalmaty@gmail.com>
2025-12-30 16:57:06 +0500

    16

--- task_manager/users/tests.py
+++ task_manager/users/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
 
+
 class UsersTests(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(
