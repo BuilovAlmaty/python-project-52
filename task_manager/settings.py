a0c7dcd727718e729b9700818bb030d1f24ddd89
Alexey Builov <builovalmaty@gmail.com>
2026-01-07 17:09:53 +0500

    Fix SECRET_KEY for tests and CI

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -27,7 +27,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv("SECRET_KEY", "test-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
