8db5865a9dd9bea4ae276f5aa3e5fdc72b551b32
Alexey Builov <builovalmaty@gmail.com>
2026-01-08 12:07:01 +0500

    tests coverage

--- task_manager/tasks/tests.py
+++ task_manager/tasks/tests.py
@@ -1,4 +1,7 @@
+from unittest.mock import patch
+
 from django.contrib.auth.models import User
+from django.db import IntegrityError
 from django.test import TestCase
 from django.urls import reverse
 
@@ -12,75 +15,145 @@ class TaskTests(TestCase):
     def log_decorator(func):
         def wrapper(self, *args, **kwargs):
             print(f'tasks: {func.__name__}')
-            result = func(self, *args, **kwargs)
-            return result
+            return func(self, *args, **kwargs)
         return wrapper
 
     def setUp(self):
         self.user = User.objects.create_user(
             username='testuser',
+            password='password',
+            first_name='Test',
+            last_name='User'
+        )
+        self.other_user = User.objects.create_user(
+            username='other',
             password='password'
         )
         self.status = TaskStatus.objects.create(name='New')
         self.label = Label.objects.create(name='Bug')
         self.client.login(username='testuser', password='password')
 
-    @log_decorator
-    def test_task_create(self):
-        data = {
-            'name': 'Test Task',
-            'description': 'Description',
-            'status': self.status.id,
-            'labels': [self.label.id],
-        }
-        response = self.client.post(reverse('tasks:create'), data)
-        self.assertEqual(response.status_code, 302)
-        task = Task.objects.get(name='Test Task')
-        self.assertEqual(task.author, self.user)
-        self.assertIn(self.label, task.labels.all())
+    # -------------------------------------------------
+    # TasksListView
+    # -------------------------------------------------
 
     @log_decorator
-    def test_task_update_view(self):
+    def test_tasks_list_view_queryset_and_context(self):
         task = Task.objects.create(
-            name='Old Task',
-            description='Old',
-            status=self.status, author=self.user
-        )
-        TaskMembership.objects.create(
-            task=task,
-            user=self.user,
-            role='creator'
-        )
-        data = {
-            'name': task.name,
-            'description': 'Updated',
-            'status': self.status.id,
-            'labels': [self.label.id],
-        }
-        response = self.client.post(
-            reverse(
-                'tasks:update',
-                args=[task.id]),
-            data
-        )
-        self.assertEqual(response.status_code, 302)
-        task.refresh_from_db()
-        self.assertEqual(task.description, 'Updated')
-        self.assertIn(self.label, task.labels.all())
-
-    @log_decorator
-    def test_task_delete_view(self):
-        task = Task.objects.create(
-            name='Delete Task',
-            description='To delete',
+            name='Task',
             status=self.status,
             author=self.user
         )
         TaskMembership.objects.create(
             task=task,
             user=self.user,
-            role='creator'
+            role='executor'
         )
-        response = self.client.post(reverse('tasks:delete', args=[task.id]))
+
+        response = self.client.get(reverse('tasks:index'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('tasks', response.context)
+        self.assertIn('filter', response.context)
+
+        tasks = response.context['tasks']
+        self.assertEqual(len(tasks), 1)
+        self.assertTrue(tasks[0].executor)
+
+    # -------------------------------------------------
+    # TasksCreateView
+    # -------------------------------------------------
+
+    @log_decorator
+    def test_task_create_with_executor(self):
+        data = {
+            'name': 'Task',
+            'description': 'Desc',
+            'status': self.status.id,
+            'executor': self.other_user.id,
+            'labels': [self.label.id],
+        }
+
+        response = self.client.post(reverse('tasks:create'), data)
         self.assertEqual(response.status_code, 302)
-        self.assertFalse(Task.objects.filter(id=task.id).exists())
+
+        task = Task.objects.get(name='Task')
+        self.assertEqual(task.author, self.user)
+
+        self.assertTrue(
+            TaskMembership.objects.filter(
+                task=task,
+                user=self.other_user,
+                role='executor'
+            ).exists()
+        )
+
+    @log_decorator
+    def test_task_create_integrity_error(self):
+        data = {
+            'name': 'Task',
+            'status': self.status.id,
+        }
+
+        with patch('task_manager.tasks.views.transaction.atomic',
+                   side_effect=IntegrityError):
+            response = self.client.post(reverse('tasks:create'), data)
+
+        self.assertEqual(response.status_code, 200)
+
+    # -------------------------------------------------
+    # TasksUpdateView
+    # -------------------------------------------------
+
+    @log_decorator
+    def test_task_update_no_permission(self):
+        task = Task.objects.create(
+            name='Task',
+            status=self.status,
+            author=self.other_user
+        )
+
+        with patch(
+            'task_manager.tasks.views.has_permission',
+            return_value=False
+        ):
+            response = self.client.post(
+                reverse('tasks:update', args=[task.id]),
+                {}
+            )
+
+        self.assertEqual(response.status_code, 302)
+
+    # -------------------------------------------------
+    # TasksDeleteView
+    # -------------------------------------------------
+
+    @log_decorator
+    def test_task_delete_no_permission(self):
+        task = Task.objects.create(
+            name='Task',
+            status=self.status,
+            author=self.other_user
+        )
+
+        response = self.client.post(
+            reverse('tasks:delete', args=[task.id])
+        )
+
+        self.assertEqual(response.status_code, 302)
+
+    @log_decorator
+    def test_task_delete_protected_error(self):
+        task = Task.objects.create(
+            name='Task',
+            status=self.status,
+            author=self.user
+        )
+
+        with patch.object(Task, 'delete',
+                          side_effect=Exception()):
+            response = self.client.post(
+                reverse('tasks:delete', args=[task.id])
+            )
+
+        self.assertEqual(response.status_code, 302)
--- task_manager/users/tests.py
+++ task_manager/users/tests.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.models import User
+from django.db.models import ProtectedError
 from django.test import TestCase
 from django.urls import reverse
+
+from task_manager.statuses.models import TaskStatus
+from task_manager.tasks.models import Task
+
+from .forms import UserUpdateForm
 
 
 class UsersTests(TestCase):
@@ -102,3 +108,119 @@ class UsersTests(TestCase):
         )
         self.assertEqual(response.status_code, 302)
         self.assertFalse(User.objects.filter(id=self.user.id).exists())
+
+    @log_decorator
+    def test_user_update_view_without_password(self):
+        response = self.client.post(
+            reverse('users:update', args=[self.user.id]),
+            {
+                'username': 'testuser',
+                'first_name': 'Updated',
+                'last_name': 'User',
+                'password1': '',
+                'password2': '',
+            }
+        )
+        self.assertEqual(response.status_code, 302)
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.first_name, 'Updated')
+
+    @log_decorator
+    def test_user_update_view_with_password(self):
+        response = self.client.post(
+            reverse('users:update', args=[self.user.id]),
+            {
+                'username': 'testuser',
+                'first_name': 'Test',
+                'last_name': 'User',
+                'password1': 'newpass1234',
+                'password2': 'newpass1234',
+            }
+        )
+        self.assertEqual(response.status_code, 302)
+        self.user.refresh_from_db()
+        self.assertTrue(self.user.check_password('newpass1234'))
+
+    @log_decorator
+    def test_update_dispatch_denies_other_user(self):
+        other = User.objects.create_user(
+            username='other',
+            password='pass'
+        )
+        self.client.login(username='other', password='pass')
+
+        response = self.client.post(
+            reverse('users:update', args=[self.user.id]),
+            {
+                'username': self.user.username,
+                'first_name': 'Hack',
+                'last_name': 'Hack',
+            }
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.user.refresh_from_db()
+        self.assertNotEqual(self.user.first_name, 'Hack')
+        self.assertEqual(other.username, 'other')
+
+    @log_decorator
+    def test_delete_dispatch_denies_other_user(self):
+        other = User.objects.create_user(
+            username='other',
+            password='pass'
+        )
+        self.client.login(username='other', password='pass')
+
+        response = self.client.post(
+            reverse('users:delete', args=[self.user.id])
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(User.objects.filter(id=self.user.id).exists())
+        self.assertEqual(other.username, 'other')
+
+    @log_decorator
+    def test_form_clean_matching_passwords(self):
+        form = UserUpdateForm(
+            instance=self.user,
+            data={
+                'username': self.user.username,
+                'first_name': self.user.first_name,
+                'last_name': self.user.last_name,
+                'new_password1': 'abc12345',
+                'new_password2': 'abc12345',
+            }
+        )
+        self.assertTrue(form.is_valid())
+
+    @log_decorator
+    def test_form_save_skips_password(self):
+        old_password = self.user.password
+
+        form = UserUpdateForm(
+            instance=self.user,
+            data={
+                'username': self.user.username,
+                'first_name': self.user.first_name,
+                'last_name': self.user.last_name,
+                'new_password1': '',
+                'new_password2': '',
+            }
+        )
+        self.assertTrue(form.is_valid())
+        form.save()
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.password, old_password)
+
+    @log_decorator
+    def test_user_delete_view_protected_error(self):
+        status = TaskStatus.objects.create(name='Test status')
+
+        Task.objects.create(
+            name='Task',
+            status=status,
+            author=self.user,
+        )
+
+        with self.assertRaises(ProtectedError):
+            self.user.delete()
